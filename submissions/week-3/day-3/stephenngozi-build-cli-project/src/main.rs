039b0d93dd8b127010204499c77a6ee8d7460c0b
Stephen Ngozi <ngozi.stephen99@gmail.com>
2026-02-12 07:34:17 +0100

    Add Week 3 Day 3 task for StephenNgozi Build CLI Project

--- submissions/week-3/day-3/stephenngozi-build-cli-project/src/main.rs
+++ submissions/week-3/day-3/stephenngozi-build-cli-project/src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::process;
+use stephenngozi_build_cli_project::Config;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err |{
+        eprintln!("Problem parsing argumemts: {}", err);
+        process::exit(1);
+    });
+
+
+    println!(" searching for {}", config.query);
+    println!("In File {}", config.filename);
+
+    if let Err(e) = stephenngozi_build_cli_project::run(config){
+        eprintln!("Application Error: {}", e);
+        process::exit(1);
+    }
+  
 }
+
+
+
+
